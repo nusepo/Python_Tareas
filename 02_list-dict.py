42f4e68215a501adfb12de44cfb00f612c1dfbe7
nusepo <paupalirubio@hotmail.com>
2019-01-22 19:46:01 +0100

    Add files via upload

---
+++ 02_list-dict.py
@@ -0,0 +1,20 @@
+presentacion = ["hola", "que", "tal", "estas", "hoy", "?"]
+print(presentacion)
+
+pi = [3, 14, 16, 0]
+print(pi)
+
+varios = [3, "a", "e", .0, "i", 3.4, "o", "u", 11]
+print(varios)
+
+ult_presentacion=presentacion[-1]
+ult_pi=pi[-1]
+ult_varios=varios[-1]
+
+print(ult_presentacion, ult_pi, ult_varios)
+
+dic = {"j.k": "harry potter", "J. R. R. Tolkien":"El Señor de los Anillos", "George R. R. Martin":"juego de tronos"}
+print(dic)
+
+print(dic.keys())
+print(dic.values())

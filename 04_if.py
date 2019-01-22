b14653e8ae9ea5a9da20766420e9b649cdadd326
nusepo <paupalirubio@hotmail.com>
2019-01-22 20:06:36 +0100

    Add files via upload

---
+++ 04_if.py
@@ -0,0 +1,11 @@
+var=int(input("Introduce un numero "))
+
+if (var<20):
+    print("el numero es", var, "pertenece al rango menor de 20")
+elif (var>=20 and var<=30):
+    print("el numero es", var, "pertenece al rango entre 20 y 39")
+elif (var>=40 and var <=59):
+    print("el numero es", var, "pertenece al rango entre 40 y 59")
+elif (var>=60):
+    print("el numero es", var, "pertenece al rango mayor de 60")
+

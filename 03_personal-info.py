257383d55823e35f7a019fae7e1d4d91aaa2bf0b
nusepo <paupalirubio@hotmail.com>
2019-01-22 19:58:08 +0100

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,16 @@
+print("Introduzca su nombre")
+nombre=input()
+
+print("Introduzca su apellido")
+apellido=input()
+
+print("Introduzca su telefono")
+telefono=input()
+
+print("Introduzca su ciudad")
+ciudad=input()
+
+print("Introduzca su edad")
+edad=int(input())
+
+print(" Hola " + nombre +" "+ apellido +" de " + ciudad +'.' + " ¿su telefono es", telefono, ", y su edad", edad, "?")
